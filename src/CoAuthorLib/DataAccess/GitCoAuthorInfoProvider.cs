6465b947758cd417db6a1866a91cdf6b4e4ccbb2
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-05-06 12:19:23 +0100

    add license headers to library

--- src/CoAuthorLib/DataAccess/GitCoAuthorInfoProvider.cs
+++ src/CoAuthorLib/DataAccess/GitCoAuthorInfoProvider.cs
@@ -1,4 +1,13 @@
-﻿using Tomlyn;
+﻿/*
+    CoAuthorLib
+    Copyright (c) Alastair Lundy 2026
+ 
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+using Tomlyn;
 
 namespace CoAuthorLib.DataAccess;
 
--- src/CoAuthorLib/Models/GitCoAuthor.cs
+++ src/CoAuthorLib/Models/GitCoAuthor.cs
@@ -1,4 +1,13 @@
-﻿namespace CoAuthorLib.Models;
+﻿/*
+    CoAuthorLib
+    Copyright (c) Alastair Lundy 2026
+ 
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+namespace CoAuthorLib.Models;
 
 // ReSharper disable once PartialTypeWithSinglePart
 public partial record GitCoAuthor
