72309b2e8753c77fb1f305fd2cabbb26b788a24b
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-05-06 12:31:58 +0100

    Create GlobalUsings.cs

---
+++ src/CoAuthor.Cli/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿// Global using directives
+
+global using CoAuthorLib.Builders;
+global using CoAuthorLib.DataAccess;
+global using CoAuthorLib.Extensions;
+global using CoAuthorLib.Models;
+global using DotMake.CommandLine;
