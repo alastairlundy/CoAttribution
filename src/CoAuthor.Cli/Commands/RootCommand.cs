ae4d1ae52c52df9c5066e07ba572474e2015df48
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-05-29 10:09:35 +0100

    start working on Root Command

--- src/CoAuthor.Cli/Commands/RootCommand.cs
+++ src/CoAuthor.Cli/Commands/RootCommand.cs
@@ -7,13 +7,29 @@
     file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+using CoAuthor.Cli.Components.Windows;
+using Terminal.Gui.App;
+
 namespace CoAuthor.Cli.Commands;
 
 [CliCommand(Name = "")]
 public class RootCommand
 {
-    
-       
-        
-        
+    public Task<int> RunAsync(CliContext context)
+    {
+        try
+        {
+            using IApplication app = Application.Create().Init();
+
+            app.Run<MainWindow>();
+
+            return Task.FromResult(0);
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
+            
+            return Task.FromException<int>(exception);
+        }
+    }
 }
---
+++ src/CoAuthor.Cli/Components/Windows/MainWindow.cs
@@ -0,0 +1,23 @@
+﻿/*
+    CoAuthorCli
+    Copyright (c) Alastair Lundy 2026
+ 
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+using Terminal.Gui.Views;
+
+namespace CoAuthor.Cli.Components.Windows;
+
+public sealed class MainWindow : Window
+{
+    public MainWindow()
+    {
+
+
+    }
+    
+    
+}
