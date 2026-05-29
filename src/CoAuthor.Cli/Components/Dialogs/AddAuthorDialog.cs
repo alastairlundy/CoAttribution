9fa3a3d53b95db46e219a62979c35cd8af3b39e2
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-05-29 10:08:40 +0100

    add missing license headers

--- src/CoAuthor.Cli/Components/Dialogs/AddAuthorDialog.cs
+++ src/CoAuthor.Cli/Components/Dialogs/AddAuthorDialog.cs
@@ -1,4 +1,13 @@
-﻿using Terminal.Gui.Input;
+﻿/*
+    CoAuthorCli
+    Copyright (c) Alastair Lundy 2026
+ 
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+using Terminal.Gui.Input;
 using Terminal.Gui.ViewBase;
 using Terminal.Gui.Views;
 
