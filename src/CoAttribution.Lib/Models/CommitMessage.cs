3ff6405d4c1518856a5dd247ee1d008554479e26
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-06-12 14:01:32 +0100

    Update CommitMessage.cs

--- src/CoAttribution.Lib/Models/CommitMessage.cs
+++ src/CoAttribution.Lib/Models/CommitMessage.cs
@@ -7,23 +7,6 @@
     file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-using System.Text;
-
 namespace CoAttribution.Lib.Models;
 
-public record CommitMessage(string Message, string Trailer)
-{
-    public override string ToString()
-    {
-        StringBuilder stringBuilder = new();
-        
-        stringBuilder.AppendLine(Message);
-
-        stringBuilder.AppendLine();
-        stringBuilder.AppendLine();
-        
-        stringBuilder.Append(Trailer);
-        
-        return stringBuilder.ToString();
-    }
-}
+public record CommitMessage(string Message, string Trailer);
