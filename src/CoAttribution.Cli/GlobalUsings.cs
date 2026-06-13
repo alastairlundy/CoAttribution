610692b63710adca0219a279e53734382ae4ed85
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-06-13 20:15:31 +0100

    Update GlobalUsings.cs

--- src/CoAttribution.Cli/GlobalUsings.cs
+++ src/CoAttribution.Cli/GlobalUsings.cs
@@ -2,11 +2,11 @@
 
 global using CoAttribution.Cli.Contexts;
 global using CoAttribution.Cli.Localizations;
-global using CoAttribution.Cli.Models;
 global using CoAttribution.Lib;
 global using CoAttribution.Lib.Abstractions;
 global using CoAttribution.Lib.Builders;
 global using CoAttribution.Lib.Extensions;
 global using CoAttribution.Lib.Models;
+global using CoAttribution.Lib.Models.DTOs;
 global using DotMake.CommandLine;
 global using Microsoft.Extensions.Configuration;
